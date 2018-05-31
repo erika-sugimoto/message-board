09b94322d6949b69062b5fd589c028671695e640
EC2 Default User <ec2-user@ip-172-31-32-232.us-east-2.compute.internal>
2018-05-31 08:05:44 +0000

    bootstrap design

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -11,5 +11,25 @@
     {!! Form::model($message, ['route' => ['messages.destroy', $message->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除') !!}
     {!! Form::close() !!}
+        <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $message->id }}</td>
+        </tr>
+        <tr>
+            <th>タイトル</th>
+            <td>{{ $message->title }}</td>
+        </tr>
+        <tr>
+            <th>メッセージ</th>
+            <td>{{ $message->content }}</td>
+        </tr>
+    </table>
+    
+     {!! link_to_route('messages.edit', 'このメッセージを編集', ['id' => $message->id], ['class' => 'btn btn-default']) !!}
+     
+       {!! Form::model($message, ['route' => ['messages.destroy', $message->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
 
-Route::get('message/', 'MessagesController@index');
+Route::get('/', 'MessagesController@index');
 Route::resource('messages', 'MessagesController');
